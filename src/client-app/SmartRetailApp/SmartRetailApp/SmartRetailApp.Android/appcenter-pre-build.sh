327bfb2bb30b2fef21aecabe9d893096c78cf5b0
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-05-13 19:52:44 +0900

    Added appcenter-pre-build.sh

---
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/appcenter-pre-build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/google-services.json
+
+echo "PATH=" $GOOGLE_JSON_FILE
+
+if [ -e "$GOOGLE_JSON_FILE" ]
+then
+    echo "Updating Google Json"
+    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
+
+    echo "File content:"
+    cat $GOOGLE_JSON_FILE
+fi
+
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
+
+if [ -e "$APP_CONSTANT_FILE" ]
+then
+    sed -i '' 's#CartsApiName = "[-A-Za-z0-9:_./]*"#CartsApiName = "'$CartsApiName'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#ApiKey = "[-A-Za-z0-9:_./]*"#ApiKey = "'$ApiKey'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#AppCenterKeyAndroid = "[-A-Za-z0-9:_./]*"#AppCenterKeyAndroid = "'$AppCenterKeyAndroid'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#AppCenterKeyiOS = "[-A-Za-z0-9:_./]*"#AppCenterKeyiOS = "'$AppCenterKeyiOS'"#' $APP_CONSTANT_FILE
+
+    echo "File content:"
+    cat $APP_CONSTANT_FILE
+fi
