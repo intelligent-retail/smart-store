19dc8dbb7df05399df031398c3e55260e0118178
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-05-13 18:53:44 +0900

    Fixed regex expression

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
@@ -3,20 +3,20 @@
     public class Constant
     {
         // APIのURL
-        public const string MainUrl = "{your_MainUrl}";
+        public const string MainUrl = "your_MainUrl";
 
         // Cart API の名前
-        public const string CartsApiName = "{your_CartsApiName}";
+        public const string CartsApiName = "your_CartsApiName";
 
         // APIのキー
-        public const string ApiKey = "{your_ApiKey}";
+        public const string ApiKey = "your_ApiKey";
 
 
         // AppCenter Android用のキー
-        public const string AppCenterKeyAndroid = "{your_AppCenterKeyAndroid}";
+        public const string AppCenterKeyAndroid = "your_AppCenterKeyAndroid";
 
         // AppCenter iOS用のキー
-        public const string AppCenterKeyiOS = "{your_AppCenterKeyiOS}";
+        public const string AppCenterKeyiOS = "your_AppCenterKeyiOS";
 
     }
 }
