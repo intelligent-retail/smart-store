df9e081c07755869813af24157195c5914eeaec0
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-05-13 18:11:59 +0900

    Added echo filename

--- src/client-app/SmartRetailApp/appcenter-pre-build.sh
+++ src/client-app/SmartRetailApp/appcenter-pre-build.sh
@@ -2,6 +2,8 @@
 
 GOOGLE_JSON_FILE=$APPCENTER_SOURCE_DIRECTORY/src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/google-services.json
 
+echo "PATH=" $GOOGLE_JSON_FILE
+
 if [ -e "$GOOGLE_JSON_FILE" ]
 then
     echo "Updating Google Json"
