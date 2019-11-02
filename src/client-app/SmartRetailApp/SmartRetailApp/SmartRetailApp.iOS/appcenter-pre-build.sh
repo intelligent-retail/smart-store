4a720364e1e19d432ff6a621ff3778ffd68ab8e2
Satoru Fujimori <satoru.fujimori@gmail.com>
2019-11-03 01:42:47 +0900

    Added appcenter-pre-build.sh

---
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.iOS/appcenter-pre-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+APP_CONSTANT_FILE=$APPCENTER_SOURCE_DIRECTORY/src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
+
+if [ -e "$APP_CONSTANT_FILE" ]
+then
+    sed -i '' 's#CartsApiName = "[-A-Za-z0-9:_./]*"#CartsApiName = "'$CartsApiName'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#ApiKey = "[-A-Za-z0-9:_./]*"#ApiKey = "'$ApiKey'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#AppCenterKeyiOS = "[-A-Za-z0-9:_./]*"#AppCenterKeyiOS = "'$AppCenterKeyiOS'"#' $APP_CONSTANT_FILE
+
+    echo "File content:"
+    cat $APP_CONSTANT_FILE
+fi
