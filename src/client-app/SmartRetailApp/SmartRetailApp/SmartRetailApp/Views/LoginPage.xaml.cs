4fac27d93c9f9113f5b7c64d3f3ddefaf9495ac3
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-01-15 16:20:01 +0900

    Test

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Views/LoginPage.xaml.cs
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Views/LoginPage.xaml.cs
@@ -23,7 +23,7 @@ namespace SmartRetailApp.Views
             edtBoxName.Text = "SmartBox1";
 
 #if AUTH
-            btnStartShopping.IsEnabled = false; xxx
+            btnStartShopping.IsEnabled = false;
             btnLoginLogout.IsVisible = true;
 #else
             btnStartShopping.IsEnabled = true;
