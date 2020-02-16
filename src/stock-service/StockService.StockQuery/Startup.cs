43f5416d90f8faf1048e3f737c41663f0869107d
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-02-17 06:26:10 +0900

    Added Key Vault configuration

--- src/stock-service/StockService.StockQuery/Startup.cs
+++ src/stock-service/StockService.StockQuery/Startup.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(StockService.StockQuery.Startup))]
@@ -9,11 +9,24 @@ namespace StockService.StockQuery
 {
     class Startup : FunctionsStartup
     {
+        public Startup()
+        {
+            var config = new ConfigurationBuilder().AddEnvironmentVariables();
+
+            var builtConfig = config.Build();
+
+            config.AddAzureKeyVault(builtConfig["KeyVault:Endpoint"]);
+
+            Configuration = config.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
             builder.Services.AddDbContext<StockDbContext>((provider, options) =>
             {
-                options.UseSqlServer(Environment.GetEnvironmentVariable("SqlConnection", EnvironmentVariableTarget.Process))
+                options.UseSqlServer(Configuration.GetConnectionString("SqlConnection"))
                        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
             });
         }
