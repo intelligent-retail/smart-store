791f2091cc612897ab31d492cf6407ed926410da
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-09 15:22:21 +0900

    Add using

--- src/box-service/BoxManagementService/BoxManagementServiceFunctions.cs
+++ src/box-service/BoxManagementService/BoxManagementServiceFunctions.cs
@@ -14,6 +14,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
 namespace BoxManagementService
 {
