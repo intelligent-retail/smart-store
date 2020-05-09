881ee4a0369204d284e12f7daca66efafcd057a1
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-09 22:15:56 +0900

    Fixed ios .sh file

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.iOS/appcenter-pre-build.sh
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.iOS/appcenter-pre-build.sh
@@ -7,6 +7,11 @@ then
     sed -i '' 's#CartsApiName = "[-A-Za-z0-9:_./]*"#CartsApiName = "'$CartsApiName'"#' $APP_CONSTANT_FILE
     sed -i '' 's#ApiKey = "[-A-Za-z0-9:_./]*"#ApiKey = "'$ApiKey'"#' $APP_CONSTANT_FILE
     sed -i '' 's#AppCenterKeyiOS = "[-A-Za-z0-9:_./]*"#AppCenterKeyiOS = "'$AppCenterKeyiOS'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#tenantName = "[-A-Za-z0-9:_./]*"#tenantName = "'$TenantName'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#tenantId = "[-A-Za-z0-9:_./]*"#tenantId = "'$TenantId'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#clientId = "[-A-Za-z0-9:_./]*"#clientId = "'$ClientId'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#policySignin = "[-A-Za-z0-9:_./]*"#policySignin = "'$PolicySignin'"#' $APP_CONSTANT_FILE
+    sed -i '' 's#iosKeychainSecurityGroup = "[-A-Za-z0-9:_./]*"#iosKeychainSecurityGroup = "'$PolicySignin'"#' $APP_CONSTANT_FILE
 
     echo "File content:"
     cat $APP_CONSTANT_FILE
