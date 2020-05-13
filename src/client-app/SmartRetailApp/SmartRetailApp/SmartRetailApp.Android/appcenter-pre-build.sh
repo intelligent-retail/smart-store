4c3d6740f4185b3650c893c954a0fbae97159d0b
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-13 09:49:59 +0900

    Fix android .sh file

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/appcenter-pre-build.sh
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/appcenter-pre-build.sh
@@ -7,7 +7,7 @@ echo "PATH=" $GOOGLE_JSON_FILE
 if [ -e "$GOOGLE_JSON_FILE" ]
 then
     echo "Updating Google Json"
-    echo "$GOOGLE_JSON" > $GOOGLE_JSON_FILE
+    echo "$GoogleJson" > $GOOGLE_JSON_FILE
     sed -i -e 's/\\"/'\"'/g' $GOOGLE_JSON_FILE
 
     echo "File content:"
