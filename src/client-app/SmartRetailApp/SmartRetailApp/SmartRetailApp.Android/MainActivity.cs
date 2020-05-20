e7e27b87cf47f680f78b1486ef7c20534c17c35e
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-20 19:06:44 +0900

    Add notification hubs for Android

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/MainActivity.cs
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.Android/MainActivity.cs
@@ -7,8 +7,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using Microsoft.AppCenter.Push;
 using ZXing.Mobile;
+using Android.Gms.Common;
+using Android.Util;
+using SmartRetailApp.Models;
 
 namespace SmartRetailApp.Droid
 {
@@ -26,12 +28,58 @@ namespace SmartRetailApp.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            if (IsPlayServiceAvailable() == false)
+            {
+                throw new Exception("This device does not have Google Play Services and cannot receive push notifications.");
+            }
+
+            CreateNotificationChannel();
         }
 
         protected override void OnNewIntent(Intent intent)
         {
+            if (intent.Extras != null)
+            {
+                var message = intent.GetStringExtra("message");
+                (App.Current as App)?.PushCartPageAsync();
+            }
+
             base.OnNewIntent(intent);
-            Push.CheckLaunchedFromNotification(this, intent);
+        }
+
+        bool IsPlayServiceAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(Constant.DebugTag, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(Constant.DebugTag, "This device is not supported");
+                }
+                return false;
+            }
+            return true;
+        }
+
+        void CreateNotificationChannel()
+        {
+            // Notification channels are new as of "Oreo".
+            // There is no need to create a notification channel on older versions of Android.
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.O)
+            {
+                var channelName = Constant.NotificationChannelName;
+                var channelDescription = String.Empty;
+                var channel = new NotificationChannel(channelName, channelName, NotificationImportance.Default)
+                {
+                    Description = channelDescription
+                };
+
+                var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+                notificationManager.CreateNotificationChannel(channel);
+            }
         }
     }
 }
--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.iOS/AppDelegate.cs
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp.iOS/AppDelegate.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-
 using Foundation;
+using SmartRetailApp.Models;
 using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
+
 
 namespace SmartRetailApp.iOS
 {
@@ -13,6 +16,8 @@ namespace SmartRetailApp.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private SBNotificationHub Hub { get; set; }
+
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -26,7 +31,110 @@ namespace SmartRetailApp.iOS
             ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             LoadApplication(new App());
 
-            return base.FinishedLaunching(app, options);
+            base.FinishedLaunching(app, options);
+
+            RegisterForRemoteNotifications();
+
+            return true;
+        }
+
+        void RegisterForRemoteNotifications()
+        {
+            // register for remote notifications based on system version
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert |
+                    UNAuthorizationOptions.Sound |
+                    UNAuthorizationOptions.Sound,
+                    (granted, error) =>
+                    {
+                        if (granted)
+                            InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                    });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Hub = new SBNotificationHub(Constant.ListenConnectionString, Constant.NotificationHubName);
+
+            // update registration with Azure Notification Hub
+            Hub.UnregisterAll(deviceToken, (error) =>
+            {
+                if (error != null)
+                {
+                    Debug.WriteLine($"Unable to call unregister {error}");
+                    return;
+                }
+
+                var tags = new NSSet(Constant.SubscriptionTags.ToArray());
+                Hub.RegisterNative(deviceToken, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        Debug.WriteLine($"RegisterNativeAsync error: {errorCallback}");
+                    }
+                });
+
+                var templateExpiration = DateTime.Now.AddDays(120).ToString(System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+                Hub.RegisterTemplate(deviceToken, "defaultTemplate", Constant.APNTemplateBody, templateExpiration, tags, (errorCallback) =>
+                {
+                    if (errorCallback != null)
+                    {
+                        if (errorCallback != null)
+                        {
+                            Debug.WriteLine($"RegisterTemplateAsync error: {errorCallback}");
+                        }
+                    }
+                });
+            });
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // make sure we have a payload
+            if (options != null && options.ContainsKey(new NSString("aps")))
+            {
+                // get the APS dictionary and extract message payload. Message JSON will be converted
+                // into a NSDictionary so more complex payloads may require more processing
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+                string payload = string.Empty;
+                NSString payloadKey = new NSString("alert");
+                if (aps.ContainsKey(payloadKey))
+                {
+                    payload = aps[payloadKey].ToString();
+                }
+
+                if (!string.IsNullOrWhiteSpace(payload))
+                {
+                    (App.Current as App)?.PushCartPageAsync();
+                }
+
+            }
+            else
+            {
+                Debug.WriteLine($"Received request to process notification but there was no payload.");
+            }
         }
     }
+
 }
