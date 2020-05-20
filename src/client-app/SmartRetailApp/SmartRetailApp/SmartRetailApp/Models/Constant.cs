c3b86827d6895e2c480d7a50a898552f0305b4d7
Satoru Fujimori <satoru.fujimori@gmail.com>
2020-05-20 17:39:33 +0900

    Add notification hub for ios

--- src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
+++ src/client-app/SmartRetailApp/SmartRetailApp/SmartRetailApp/Models/Constant.cs
@@ -15,5 +15,17 @@
         // AppCenter iOS用のキー
         public const string AppCenterKeyiOS = "your_AppCenterKeyiOS";
 
+        // Azure Portal の Notification Hub Namespace の名前
+        public static string NotificationHubName { get; set; } = "YourNotificationHubName";
+
+        // 接続文字列
+        // Azure Portal → Notification Hub → Access Policies → DefaultListenShared AccessSignature
+        // ※ Listen のみの接続文字列でないと動作しないので注意
+        public static string ListenConnectionString { get; set; } = "YourListenConnectionString";
+
+        // 登録するタグ
+        public static string[] SubscriptionTags { get; set; } = { "default", "yoga" };
+
+        public static string APNTemplateBody { get; set; } = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
     }
 }
