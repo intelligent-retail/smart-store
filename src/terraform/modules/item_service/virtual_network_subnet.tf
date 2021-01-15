25d9d128690d4853b474802a41a9e65611f7751f
Kazumi OHIRA <dzeyelid@gmail.com>
2021-01-15 20:56:50 +0900

    Update terraform script

--- src/terraform/modules/item_service/virtual_network_subnet.tf
+++ src/terraform/modules/item_service/virtual_network_subnet.tf
@@ -1,10 +1,9 @@
 resource "azurerm_subnet" "item_service" {
-  name                                           = "snet-${local.identifier_in_module}"
-  resource_group_name                            = var.resource_group.name
-  virtual_network_name                           = data.azurerm_virtual_network.shared.name
-  address_prefixes                               = [var.snet_address_prefix]
-  enforce_private_link_endpoint_network_policies = true
-  service_endpoints                              = ["Microsoft.Storage", "Microsoft.AzureCosmosDB", "Microsoft.Web"]
+  name                 = "snet-${local.identifier_in_module}"
+  resource_group_name  = var.resource_group.name
+  virtual_network_name = data.azurerm_virtual_network.shared.name
+  address_prefixes     = [var.snet_address_prefix]
+  service_endpoints    = ["Microsoft.Storage", "Microsoft.AzureCosmosDB", "Microsoft.Web"]
 
   delegation {
     name = "Delegate to app plan"
